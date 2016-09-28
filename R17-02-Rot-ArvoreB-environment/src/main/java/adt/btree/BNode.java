29a10c3fdb7e8f734300f36df957b142c09ac3c8
lucasfoliveira <lucas.oliveira@ccc.ufcg.edu.br>
2016-09-28 01:36:03 -0300

    Not Shame

--- R17-02-Rot-ArvoreB-environment/src/main/java/adt/btree/BNode.java
+++ R17-02-Rot-ArvoreB-environment/src/main/java/adt/btree/BNode.java
@@ -1,21 +1,24 @@
 package adt.btree;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 public class BNode<T extends Comparable<T>> {
-	protected LinkedList<T> elements; //PODERIA TRABALHAR COM ARRAY TAMBEM
-	protected LinkedList<BNode<T>> children; //PODERIA TRABALHAR COM ARRAY TAMBEM
+	protected LinkedList<T> elements; // PODERIA TRABALHAR COM ARRAY TAMBEM
+	protected LinkedList<BNode<T>> children; // PODERIA TRABALHAR COM ARRAY
+												// TAMBEM
 	protected BNode<T> parent;
 	protected int maxKeys;
 	protected int maxChildren;
-	
-	public BNode(int order){
+
+	public BNode(int order) {
 		this.maxChildren = order;
 		this.maxKeys = order - 1;
-		this.elements = new LinkedList<T>();	
+		this.elements = new LinkedList<T>();
 		this.children = new LinkedList<BNode<T>>();
 	}
+
 	@Override
 	public String toString() {
 		return this.elements.toString();
@@ -24,12 +27,12 @@ public class BNode<T extends Comparable<T>> {
 	@Override
 	public boolean equals(Object obj) {
 		boolean resp = false;
-		if(obj != null){
-			if(obj instanceof BNode){
-				if(this.size() == ((BNode<T>) obj).size()){
+		if (obj != null) {
+			if (obj instanceof BNode) {
+				if (this.size() == ((BNode<T>) obj).size()) {
 					resp = true;
 					int i = 0;
-					while(i<this.size() && resp){
+					while (i < this.size() && resp) {
 						resp = resp && this.getElementAt(i).equals(((BNode<T>) obj).getElementAt(i));
 						i++;
 					}
@@ -38,91 +41,268 @@ public class BNode<T extends Comparable<T>> {
 		}
 		return resp;
 	}
-	public boolean isEmpty(){
+
+	public boolean isEmpty() {
 		return this.size() == 0;
 	}
-	public int size(){
+
+	public int size() {
 		return this.elements.size();
 	}
-	public boolean isLeaf(){
+
+	public boolean isLeaf() {
 		return this.children.size() == 0;
 	}
-	public boolean isFull(){
-		return this.size()== maxKeys;
+
+	public boolean isFull() {
+		return this.size() == maxKeys;
 	}
-	public void addElement(T element){
+
+	public void addElement(T element) {
 		this.elements.add(element);
 		Collections.sort(elements);
 	}
-	public void removeElement(T element){
+
+	public void removeElement(T element) {
 		this.elements.remove(element);
 	}
-	public void removeElement(int position){
+
+	public void removeElement(int position) {
 		this.elements.remove(position);
 	}
-	public void addChild(int position, BNode<T> child){
+
+	public void addChild(int position, BNode<T> child) {
 		this.children.add(position, child);
 		child.parent = this;
 	}
-	public void removeChild(BNode<T> child){
+
+	public void removeChild(BNode<T> child) {
 		this.children.remove(child);
 	}
-	public int indexOfChild(BNode<T> child){
+
+	public int indexOfChild(BNode<T> child) {
 		return this.children.indexOf(child);
 	}
-	public T getElementAt(int index){
+
+	public T getElementAt(int index) {
 		return this.elements.get(index);
 	}
-	protected void split(){
-		//TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+	private void guardaElementos(T mediana, BNode<T> maior, BNode<T> menor) {
+
+		int index = 0;
+
+		while (index < getElements().size()) {
+
+			if (mediana.compareTo(getElementAt(index)) < 0) {
+				maior.addElement(getElementAt(index));
+			}
+
+			if (mediana.compareTo(getElementAt(index)) > 0) {
+				menor.addElement(getElementAt(index));
+			}
+
+			index++;
+		}
 	}
-	protected void promote(){
-		//TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+	private void reorganizador(LinkedList<BNode<T>> filhos, BNode<T> node, int first, int last) {
+
+		int pos;
+		int index = first;
+
+		while (index < last) {
+			pos = position(node.getElements(), filhos.get(index).getElements().get(0));
+			node.addChild(pos, filhos.get(index));
+
+			index++;
+		}
 	}
+
+	private int position(LinkedList<T> lista, T mediana) {
+
+		int i = 0;
+		while (i < lista.size()) {
+
+			if (lista.get(i).compareTo(mediana) > 0) {
+				return i;
+			}
+
+			i++;
+		}
+
+		return lista.size();
+	}
+
+	protected void split() {
+
+		T mediana = getElements().get(getElements().size() / 2);
+
+		int pos, left, right;
+
+		BNode<T> maior = new BNode<T>(getMaxChildren());
+		BNode<T> menor = new BNode<T>(getMaxChildren());
+
+		LinkedList<BNode<T>> filhos = new LinkedList<BNode<T>>();
+
+		guardaElementos(mediana, maior, menor);
+
+		if (getParent() == null && !isLeaf()) {
+
+			filhos = getChildren();
+
+			setElements(new LinkedList<T>());
+			addElement(mediana);
+			setChildren(new LinkedList<BNode<T>>());
+
+			addChild(0, menor);
+			addChild(1, maior);
+
+			reorganizador(filhos, menor, 0, menor.size() + 1);
+			reorganizador(filhos, maior, maior.size() + 1, filhos.size());
+
+		} else if (getParent() == null && isLeaf()) {
+
+			setElements(new LinkedList<T>());
+			addElement(mediana);
+			addChild(0, menor);
+			addChild(1, maior);
+
+		} else if (isLeaf()) {
+
+			BNode<T> node = new BNode<>(getMaxChildren());
+
+			node.getElements().add(mediana);
+			node.parent = getParent();
+
+			menor.parent = getParent();
+			maior.parent = getParent();
+
+			pos = position(node.getParent().getElements(), mediana);
+			left = pos;
+			right = pos + 1;
+
+			getParent().getChildren().set(left, menor);
+			getParent().getChildren().add(right, maior);
+
+			node.promote();
+
+		} else {
+
+			filhos = getChildren();
+
+			BNode<T> node = new BNode<>(getMaxChildren());
+
+			node.getElements().add(mediana);
+			node.parent = getParent();
+
+			menor.parent = getParent();
+			maior.parent = getParent();
+
+			pos = position(node.getElements(), mediana);
+			left = pos;
+			right = pos + 1;
+
+			getParent().getChildren().add(left, menor);
+			getParent().getChildren().add(right, maior);
+
+		}
+	}
+
+	protected void promote() {
+
+		int pos = position(getParent().getElements(), getElementAt(0));
+
+		getParent().getElements().add(pos, getElementAt(0));
+
+		if (getParent().size() > getMaxKeys()) {
+			getParent().split();
+		}
+
+	}
+
 	public LinkedList<T> getElements() {
 		return elements;
 	}
+
 	public void setElements(LinkedList<T> elements) {
 		this.elements = elements;
 	}
+
 	public LinkedList<BNode<T>> getChildren() {
 		return children;
 	}
+
 	public void setChildren(LinkedList<BNode<T>> children) {
 		this.children = children;
 	}
-	public BNode<T> copy(){
+
+	public BNode<T> copy() {
 		BNode<T> result = new BNode<T>(maxChildren);
 		result.parent = parent;
 		for (int i = 0; i < this.elements.size(); i++) {
 			result.addElement(this.elements.get(i));
 		}
 		for (int i = 0; i < this.children.size(); i++) {
-			result.addChild(i,((BNode<T>)this.children.get(i)).copy());
+			result.addChild(i, ((BNode<T>) this.children.get(i)).copy());
 		}
-		
+
 		return result;
 	}
+
 	public BNode<T> getParent() {
 		return parent;
 	}
+
 	public void setParent(BNode<T> parent) {
 		this.parent = parent;
 	}
+
 	public int getMaxKeys() {
 		return maxKeys;
 	}
+
 	public void setMaxKeys(int maxKeys) {
 		this.maxKeys = maxKeys;
 	}
+
 	public int getMaxChildren() {
 		return maxChildren;
 	}
+
 	public void setMaxChildren(int maxChildren) {
 		this.maxChildren = maxChildren;
 	}
 	
+	public void print() {
+		printTree(this);
+	}
 	
+	public void printTree(BNode<?> node) {
+		class Pair {
+			BNode<?> node;
+			int c;
+			boolean hasLeft;
+			Pair(BNode<?> node, int c, boolean hl) { this.node = node; this.c = -c; this.hasLeft = hl;}
+		};
+		LinkedList<Pair> lista = new LinkedList<>();
+		HashSet<BNode<?>> visitados = new HashSet<>();
+		
+		lista.add(new Pair(node, 1, false));
+		int ultimaCor = -1;
+		while (!lista.isEmpty()) {
+			Pair top = lista.removeFirst();
+			if (top.c != ultimaCor)
+				System.out.println();
+			
+			System.out.print(top.node + (top.hasLeft ? "=" : " "));
+			
+			for (BNode<?> b : top.node.getChildren()) { 
+				if (visitados.add(b))
+					lista.addLast(new Pair(b, top.c, !b.equals(top.node.getChildren().getLast()))); 
+			}
+			ultimaCor = top.c;
+		}
+		System.out.println();
+	}
 }
--- R17-02-Rot-ArvoreB-environment/src/main/java/adt/btree/BTreeImpl.java
+++ R17-02-Rot-ArvoreB-environment/src/main/java/adt/btree/BTreeImpl.java
@@ -1,5 +1,7 @@
 package adt.btree;
 
+import java.util.LinkedList;
+
 public class BTreeImpl<T extends Comparable<T>> implements BTree<T> {
 
 	protected BNode<T> root;
@@ -22,67 +24,141 @@ public class BTreeImpl<T extends Comparable<T>> implements BTree<T> {
 
 	@Override
 	public int height() {
-		return height(this.root);
+		return height(getRoot());
 	}
 
 	private int height(BNode<T> node) {
 		if (node.isEmpty()) {
 			return 0;
 		} else {
-			return 1 + height(node.getChildren().getFirst());
+			int height = 0;
+			
+			if (!node.isLeaf()) {
+				height = 1 + height(node.getChildren().get(0));
+			}
+			
+			return height;
 		}
 	}
 
 	@Override
 	public BNode<T>[] depthLeftOrder() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not Implemented yet!");
+
+		LinkedList<BNode<T>> lista = new LinkedList<BNode<T>>();
+
+		lista = depthLeftOrder(getRoot(), lista);
+
+		BNode<T>[] array = new BNode[lista.size()];
+
+		return lista.toArray(array);
+	}
+
+	public LinkedList<BNode<T>> depthLeftOrder(BNode<T> node, LinkedList<BNode<T>> lista) {
+
+		if (!node.isEmpty()) {
+
+			lista.add(node);
+
+			for (BNode<T> element : node.getChildren()) {
+				depthLeftOrder(element, lista);
+			}
+
+		}
+
+		return lista;
 	}
 
 	@Override
 	public int size() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		return size(getRoot());
+	}
+
+	private int size(BNode<T> node) {
+		if (node.isEmpty()) {
+			return 0;
+			
+		} else {
+			
+			int size = node.size();
+			
+			if (!node.isLeaf()) {
+				for (BNode<T> nodeChildren : node.getChildren()) {
+					size += size(nodeChildren);
+				}
+			}
+			
+			return size;
+		}
 	}
 
 	@Override
 	public BNodePosition<T> search(T element) {
-		return search(root, element);
+		return search(getRoot(), element);
 	}
-	
+
 	private BNodePosition<T> search(BNode<T> node, T element) {
 		int index = 1;
-		
+
 		while (index <= node.size() && element.compareTo(node.getElementAt(index)) > 0) {
 			index += 1;
 		}
-		
+
 		if (index <= node.size() && element == node.getElementAt(index)) {
 			return new BNodePosition<T>(node, index);
 		}
-		
+
 		if (node.isLeaf()) {
-			return null;
+			return new BNodePosition<T>();
 		}
-		
+
 		return search(node.getChildren().get(index), element);
+	}
+
+	private int position(LinkedList<T> list, T mediana) {
+
+		int i = 0;
+		while (i < list.size()) {
+
+			if (list.get(i).compareTo(mediana) > 0) {
+				return i;
+			}
+
+			i++;
+		}
+
+		return list.size();
 	}
 
 	@Override
 	public void insert(T element) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		if (element != null) {
+			insert(getRoot(), element);
+		}
+	}
 
+	private void insert(BNode<T> node, T element) {
+		if (node.isLeaf()) {
+
+			node.addElement(element);
+
+			if (node.getElements().size() > node.getMaxKeys()) {
+				split(node);
+			}
+
+		} else {
+
+			int pos = position(node.getElements(), element);
+			insert(node.getChildren().get(pos), element);
+
+		}
 	}
 
 	private void split(BNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		node.split();
 	}
 
 	private void promote(BNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		node.promote();
 	}
 
 	// NAO PRECISA IMPLEMENTAR OS METODOS ABAIXO
@@ -103,5 +179,4 @@ public class BTreeImpl<T extends Comparable<T>> implements BTree<T> {
 		// NAO PRECISA IMPLEMENTAR
 		throw new UnsupportedOperationException("Not Implemented yet!");
 	}
-
 }
